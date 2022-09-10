0f4da53bb04c1ca71905a8bae299e71f44545dc7
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-09-10 19:21:31 -0300

    Add files via upload

---
+++ assets/preload/stages/alien.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	-- background shit
+
+	makeLuaSprite('alien', 'alien', -300, 30);
+	setLuaSpriteScrollFactor('alien', 0.8, 0.8);
+	scaleObject('alien', 0.8, 0.8);
+                addLuaSprite('alien',false)
+
+end
